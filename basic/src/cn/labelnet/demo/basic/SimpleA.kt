6cdbe8f24412a28a027029d54e7308e933020718
yuan <1406046087@qq.com>
2017-05-23 19:27:58 +0800

    simple kotlin

---
+++ basic/src/cn/labelnet/demo/basic/SimpleA.kt
@@ -0,0 +1,11 @@
+package cn.labelnet.demo.basic
+
+// main
+fun main(args: Array<String>) {
+    sumPrint(1, 2)
+}
+
+// method : sumPrint
+fun sumPrint(a: Int, b: Int) {
+    println("a+b=${a + b}")
+}
