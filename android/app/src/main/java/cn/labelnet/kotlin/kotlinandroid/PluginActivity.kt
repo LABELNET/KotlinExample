4f80359c0f08461166f2ae38084f8c8bb3525ab5
yuan <1406046087@qq.com>
2017-07-05 16:35:15 +0800

    write plugin page

--- android/app/src/main/java/cn/labelnet/kotlin/kotlinandroid/PluginActivity.kt
+++ android/app/src/main/java/cn/labelnet/kotlin/kotlinandroid/PluginActivity.kt
@@ -2,6 +2,8 @@ package cn.labelnet.kotlin.kotlinandroid
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_plugin.*
 
 /**
  * Created by yuan on 05/07/2017.
@@ -9,9 +11,17 @@ import android.support.v7.app.AppCompatActivity
  */
 class PluginActivity : AppCompatActivity() {
 
+    var value = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_plugin);
+        setContentView(R.layout.activity_plugin)
+
+    }
+
+    fun randomClick(view: View) {
+        value++
+        tvRandom.text = "random >> ${value}"
     }
 
 }
