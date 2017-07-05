07650d656646d06a925dc4fc9bf002ff3ad0cc72
yuan <1406046087@qq.com>
2017-07-05 15:47:10 +0800

    write activity main view

--- android/app/src/main/java/cn/labelnet/kotlin/kotlinandroid/MainActivity.kt
+++ android/app/src/main/java/cn/labelnet/kotlin/kotlinandroid/MainActivity.kt
@@ -2,11 +2,43 @@ package cn.labelnet.kotlin.kotlinandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var value = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * + 1
+     */
+    fun onAddOne(view: View) {
+        value++
+        setTvValue()
+    }
+
+    /**
+     * -1
+     */
+    fun onMinusOne(view: View) {
+        value--
+        setTvValue()
+    }
+
+    /**
+     * upd value
+     */
+    fun setTvValue() {
+        val tvValue = findViewById(R.id.tvValue) as TextView
+        tvValue.text = getString(R.string.kotlin_value, value)
+        if (value == 0) {
+            Toast.makeText(this, getString(R.string.kotlin_value, value), Toast.LENGTH_LONG).show()
+        }
     }
 }
